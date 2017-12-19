8534d09ad91307aa9575b320d6cd9c0ed8b8b2f2
justasmal <justasmal@outlook.com>
2017-12-19 10:27:09 +0200

    datepicker and custom search bar added
    Signed-off-by: justasmal <justasmal@outlook.com>

---
+++ src/main/java/com/justas/planuotojaspro/code/Database.java
@@ -0,0 +1,4 @@
+package com.justas.planuotojaspro.code;
+
+public class Database {
+}
---
+++ src/main/java/com/justas/planuotojaspro/windows/Task.java
@@ -0,0 +1,15 @@
+package com.justas.planuotojaspro.windows;
+
+import javax.swing.*;
+
+public class Task {
+
+    private JLabel taskName;
+    private JLabel taskTime;
+    private JButton changeTaskStat;
+    private JButton taskSettings;
+
+    public Task(String taskname, String tasktime) {
+
+    }
+}
